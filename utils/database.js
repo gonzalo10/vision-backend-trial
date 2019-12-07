be33fece48f492cd25cc48d4eb07f16afc6843ca
gonzalo10 <gonzalo.achiaga@gmail.com>
2019-12-07 18:54:37 +0100

    feat: added aws rds direction

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,22 @@
 const Sequelize = require('sequelize');
 
-// const sequelize = new Sequelize('vision', process.env.AWS_RDS_DB_USER, process.env.AWS_RDS_DB_PASSWORD, {
-// 	host: process.env.AWS_RDS_DB_HOST ,
+const sequelize = new Sequelize(
+	'vision',
+	process.env.AWS_RDS_DB_USER,
+	process.env.AWS_RDS_DB_PASSWORD,
+	{
+		host: process.env.AWS_RDS_DB_HOST,
+		dialect: 'mysql',
+		pool: {
+			max: 10,
+			min: 0,
+			acquire: 30000,
+			idle: 10000,
+		},
+		charset: 'utf8mb4',
+	}
+);
+// const sequelize = new Sequelize('vision', 'root', 'root', {
 // 	dialect: 'mysql',
 // 	pool: {
 // 		max: 10,
@@ -11,15 +26,5 @@ const Sequelize = require('sequelize');
 // 	},
 // 	charset: 'utf8mb4',
 // });
-const sequelize = new Sequelize('vision', 'root', 'root', {
-	dialect: 'mysql',
-	pool: {
-		max: 10,
-		min: 0,
-		acquire: 30000,
-		idle: 10000,
-	},
-	charset: 'utf8mb4',
-});
 
 module.exports = sequelize;
